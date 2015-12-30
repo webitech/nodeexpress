8a07621831035c79f195fd76ba84b22a2303d0f0
alekss <aleksander.sztwiertnia@jbr.pl>
2015-12-30 15:44:00 +0100

    separate task for html reload

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@
         jscs = require('gulp-jscs'),
         nodemon = require('gulp-nodemon'),
         jsFiles = ['*.js', 'src/**/*.js'],
-        sourceFiles = ['*.js', 'src/**/*.js', 'src/views/*.*'],
+        htmlFiles = ['*.js', 'src/**/*.js', 'src/views/*.*'],
         livereload = require('gulp-livereload');
 
     gulp.task('style', function () {
@@ -39,24 +39,34 @@
             .pipe(wiredep(options))
             .pipe(inject(injectSrc, injectOptions))
             .pipe(gulp.dest('./src/views'));
-    });   
+    });
+
+    gulp.task('reload_html', function () {
+
+        livereload.listen({start: true});
+        
+        var watch = require('gulp-watch');
+        
+        watch(htmlFiles, function () {
+            console.log('HTML files changed...');
+            livereload.reload();
+        });
+        
+    });
     
-    gulp.task('serve', ['style', 'inject'], function () {
+    gulp.task('serve', ['style', 'inject', 'reload_html'], function () {
         var options = {
                 script: 'app.js',
                 delayTime: 1,
                 env: {
                     'PORT': 3000
                 },
-                watch: sourceFiles
+                watch: jsFiles
             };
-        
-        livereload.listen({start: true});
         
         return nodemon(options)
             .on('restart', function (ev) {
                 console.log('*************************** Restarting *************************** ');
-                livereload.reload();
             });
     });
     
